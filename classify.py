7fcb9b2d2fdc8bc3c619b1c0a5ca8d3cabb6a90b
Ubuntu <ubuntu@ip-172-31-59-172.ec2.internal>
2017-11-16 06:57:07 +0000

    作成

--- classify.py
+++ classify.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -12,11 +14,9 @@ from urllib2 import urlopen
 import numpy as np
 import imghdr
 
-def get_image_data(url=None, img_path=None):
+def get_image_data(img_path=None):
   raw_image = None
-  if url != None:
-    raw_image = urlopen(url).read()
-  elif img_path != None:
+  if img_path != None:
     raw_image = tf.read_file(img_path)
     with open(img_path, 'rb') as fp:
       imagedata = fp.read()
@@ -26,13 +26,10 @@ def get_image_data(url=None, img_path=None):
     if imagetype in ["png"]:
       return tf.image.decode_png(raw_image, channels=3) 
   return tf.image.decode_image(raw_image, channels=3)
-    
-slim = tf.contrib.slim
 
 def result(checkpoint_path=None, model_name=None, num_classes=0, img_path=None):
+  os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
   with tf.Graph().as_default():
-    tf_global_step = slim.get_or_create_global_step()
-    
     ####################
     # Select the model #
     ####################
@@ -50,7 +47,7 @@ def result(checkpoint_path=None, model_name=None, num_classes=0, img_path=None):
         is_training=False)
 
     eval_image_size = network_fn.default_image_size
-    sample_image = get_image_data(img_path=img_path)
+    sample_image = get_image_data(img_path = img_path)
     image = image_preprocessing_fn(sample_image, eval_image_size, eval_image_size)
     image  = tf.expand_dims(image, 0)
 
@@ -71,6 +68,41 @@ def result(checkpoint_path=None, model_name=None, num_classes=0, img_path=None):
         saver.restore(sess, checkpoint_path)
         proba = sess.run(probabilities)[0]
         result = np.array([[i, p] for i, p in zip(range(num_classes), proba)])
-    print(np.argsort(result,axis=0)[::-1][0,1])
 
     return result
+
+def main(args):
+    model_dir = args.model_dir
+    image_dir = args.image_dir
+    network_name = args.network_name
+    print("...")
+    images = os.listdir(image_dir)
+    f = open(os.path.join(model_dir, 'label.txt'))
+    lines2 = f.readlines()
+    f.close()
+    num_classes = len(lines2)
+    label_number_name={}
+    for line in lines2:
+        number, name =line.split(":", 1)
+        label_number_name[number] = name.replace('\n','')
+    iteration = os.path.basename(model_dir)[6:]
+    for image in images:
+        results = result(checkpoint_path = os.path.join(model_dir,"model.ckpt-{}".format(iteration)),
+                        model_name = network_name,
+                        num_classes = num_classes,
+                        img_path = os.path.join(image_dir, image))
+        tmp = results[np.argsort(results,axis=0)[::-1][0,1]]
+        print("{}\t{}\t{}".format(os.path.join(image_dir, image),label_number_name[str(int(tmp[0]))],tmp[1]))
+
+def get_parser():
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Labellio Classifier")
+    parser.add_argument("model_dir", help="(exp. model-1000 :this number is the number of iteration) Path to a model directory which is exported from Labellio. (Please extract the archive before you use it.)")
+    parser.add_argument("image_dir", help="Path to an image directory. The directory should contain only images.")
+    parser.add_argument("network_name", help="Network name of the model.")
+
+    return parser
+
+if __name__ == "__main__":
+    main(get_parser().parse_args())
